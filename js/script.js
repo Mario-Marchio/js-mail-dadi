ba4cef8d113ff559ca87bd2067e1bd0c2e80c23c
Mario-Marchio <mariomarchio1610@gmail.com>
2023-05-31 16:39:28 +0200

    es 2 done

--- js/script.js
+++ js/script.js
@@ -14,18 +14,22 @@ for( i = 0 ; i < email.length ; i++){
 }
 
 
-// let dado1 = 0 ;
+let dado1 = 0 ;
 
-// let dado2 = 0 ;
+let dado2 = 0 ;
 
-// dado1 = Math.floor(Math.random() * 6) + 1 ;
+dado1 = Math.floor(Math.random() * 6) + 1 ;
 
-// dado2 = Math.floor(Math.random() * 6) + 1 ;
+dado2 = Math.floor(Math.random() * 6) + 1 ;
 
-// console.log(dado1);
+console.log(dado1);
 
-// console.log(dado2);
+console.log(dado2);
 
-// if( dado1 > dado2 ){
-//     console.log(`hai vinto`);
-// }else if (dado)
+if( dado1 > dado2 ){
+    console.log(`ha vinto l'utente`);
+}else if (dado2 > dado1){
+    console.log(`ha vinto il computer`);
+}else{
+    console.log(`pareggio`);
+}
