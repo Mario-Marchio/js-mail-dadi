010cea041bd8bffeee2d39f93f88c3fad7ea66df
Mario-Marchio <mariomarchio1610@gmail.com>
2023-05-31 16:20:00 +0200

    first try for the javascript

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 console.log(`JS OK`);
 
-const email =[`mario@gmail.com`, `carlo@gmail.com`, `chiara@gmail.com`, `paolo@gmail.com` ];
+const email =[ `carlo@gmail.com`, `chiara@gmail.com`, `paolo@gmail.com`, `mario@gmail.com` ];
 console.log(email);
+
+const userEmail = prompt(`inserisci la tua email`, `mario@gmail.com`)
+console.log(userEmail);
+for( i = 0 ; i = email.length ; i++){
+    if(userEmail===email[i]){
+        allert(`benvenuto`)
+    }else {
+        console.log(`non puoi accedere`);
+    }
+}
